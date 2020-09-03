1322e6cd6dcacd373171d4e153b06567145be3cb
luis torres <tl224627@gmail.com>
2020-09-03 07:54:07 -0600

    Add project files.

---
+++ SubExample/SubExample.vb
@@ -0,0 +1,55 @@
+﻿Option Strict On
+Option Explicit On
+
+
+Module SubExample
+
+    Sub Main()
+        Dim x As Integer
+        Dim y As Integer
+        'this code runs at start up
+        Console.WriteLine("In Sub Main")
+
+        ''call the Example sub
+        'AsubExample()
+
+        'Console.WriteLine("Back In Sub Main")
+
+
+
+        'SubWithArguments("secret code")
+        'Console.ReadLine()
+        x = 5I
+        y = 3I
+        Console.WriteLine(x)
+
+        Console.ReadLine()
+        VariableSope()
+        Console.WriteLine("Back to sub main")
+        Console.WriteLine(x)
+        Console.ReadLine()
+    End Sub
+
+
+    Sub AsubExample()
+        'this code only run when  its "called"
+        Console.WriteLine("In Sub AsubExample")
+        Console.ReadLine()
+    End Sub
+
+    Sub SubWithArguments(message As String)
+
+        Console.WriteLine(message)
+
+    End Sub
+
+    Sub VariableSope()
+        Dim x As Integer
+        Console.WriteLine("In Sub VariableSope")
+
+        x = 7I
+        'y=3I
+        Console.WriteLine(x)
+        Console.ReadLine()
+    End Sub
+End Module
